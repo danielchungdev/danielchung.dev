b70fa76127f8aa5a4121d717c6c7d771d08ca1c8
danielchungdev <pikachungg@gmail.com>
2024-04-01 11:20:38 -0400

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -38,7 +38,8 @@ const Home = () => {
               height={125}
             />
             <h1 className="my-4 font-bold text-2xl text-white">Daniel Chung</h1>
-            <h1 className="text-white">Recent Web & Mobile Computing graduate from RIT, experienced in web development. Currently working at Cognivue as a Front-end Engineer.</h1>
+            <h1 className="text-white">Recent Web & Mobile Computing graduate from RIT, experienced in web development.</h1>
+            <h1 className="text-white">Currently working at Cognivue as a Front-end Engineer.</h1>
             <div className="my-4 flex gap-2">
               <a href="https://github.com/danielchungdev" target="_blank">
                 <FaGithubSquare className="text-neutral-200 w-8 h-8" />

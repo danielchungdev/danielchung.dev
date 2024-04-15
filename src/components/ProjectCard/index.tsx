3c2276cf49f295cbf9b96f90639d3b5330c8cddd
danielchungdev <pikachungg@gmail.com>
2024-04-15 14:58:24 -0400

    website update

--- src/components/ProjectCard/index.tsx
+++ src/components/ProjectCard/index.tsx
@@ -3,19 +3,22 @@ import { HoverAnimations } from "../HoverAnimation/HoverAnimation"
 
 export const ProjectCard: ProjectCardComponent = ({ project }) => {
   return (
+    <a href={project.website} target="_blank">
 
-    <HoverAnimations>
-      <div className=" text-white">
-        <h1 className="font-semibold text-lg"><a href={project.website} target="_blank">{project.name}</a></h1>
-        <div className="flex gap-2 my-2">
-          {
-            project.tools.map((tool, index) => (
-              <p key={index} className="text-sm  px-2 rounded-full border border-neutral-100">{tool}</p>
-            ))
-          }
+      <HoverAnimations>
+        <div className=" text-white">
+          <h1 className="font-semibold text-lg">{project.name}</h1>
+          <div className="flex gap-2 my-2">
+            {
+              project.tools.map((tool, index) => (
+                <p key={index} className="text-sm  px-2 rounded-full border border-neutral-100">{tool}</p>
+              ))
+            }
+          </div>
+          <p className="text-sm text-neutral-200">{project.description}</p>
         </div>
-        <p className="text-sm text-neutral-200">{project.description}</p>
-      </div>
-    </HoverAnimations>
+      </HoverAnimations>
+    </a>
+
   )
 }

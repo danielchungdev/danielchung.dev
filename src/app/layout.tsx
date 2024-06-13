7585a48b33e1cdf4f8342b084f736ccc49d403ff
danielchungdev <pikachungg@gmail.com>
2024-06-13 12:32:49 -0400

    removing overflow

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children,}: { children: React.ReactNode}) {
   return (
     <html lang="en">
-      <body className={`${inter.className} background h-screen w-screen`}>
+      <body className={`${inter.className} background h-screen w-screen overflow-x-hidden`}>
         <div className='w-screen'>
           <div className='m-auto w-full sm:w-3/4 md:w-3/5 lg:w-1/2 xl:w-1/3 2xl:w-2/6 '>
             {children}

f9d0b95358281ba4a7eb2100d7ea25dbc9fe5e17
danielchungdev <pikachungg@gmail.com>
2024-06-16 11:02:13 -0400

    updates

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Daniel Chung | SWE',
-  description: 'Portfolio website :)',
+  description: 'Daniel Chung\'s Portfolio website :)',
 }
 
 export default function RootLayout({ children,}: { children: React.ReactNode}) {

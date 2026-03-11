d1a38d57667115999fb7e50170909ef1aca5e3a8
Daniel Chung <pikachungg@gmail.com>
2026-03-11 03:17:40 -0500

    readding the light/dark mode button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,5 @@
-import TypingName from './TypingName'
-import AnimatedContent from './AnimatedContent'
+import ThemeWrapper from './ThemeWrapper'
 
 export default function Home() {
-  return (
-    <main className="max-w-2xl mx-auto px-6 py-16">
-      <h1 className="font-bold text-2xl mb-24"><TypingName /></h1>
-      <hr className="border-neutral-700 dark:border-neutral-600 mb-12 -ml-[calc(50vw-50%)] w-[50vw]" />
-      <AnimatedContent />
-    </main>
-  )
+  return <ThemeWrapper />
 }
